fd1da1a1ad05910b6d372dde9882bc5b69a9224c
VICEDOMINI Riccardo <ric.vice@gmail.com>
2022-07-27 11:03:22 +0200

    added help message, option for minimum number of samples, possibility to read from stdin

--- km_basic_filter.c
+++ km_basic_filter.c
@@ -9,17 +9,24 @@
 
 int main(int argc, char **argv) {
 
-  int min_count = 5;
+  int min_samples = 10, min_abund = 10;
   char *out_fname = NULL;
+  bool help_opt = false;
 
   int c;
-  while ((c = getopt(argc, argv, "c:o:")) != -1) {
+  while ((c = getopt(argc, argv, "a:c:o:h")) != -1) {
     switch (c) {
+      case 'a':
+        min_abund = strtol(optarg, NULL, 10);
+        break;
       case 'c':
-        min_count = strtol(optarg, NULL, 10);
+        min_samples = strtol(optarg, NULL, 10);
         break;
       case 'o':
         out_fname = optarg;
+        break;
+      case 'h':
+        help_opt = true;
         break;
       case '?':
         return 1;
@@ -28,17 +35,33 @@ int main(int argc, char **argv) {
     }
   }
 
-  if(argc-optind != 1) {
-    fprintf(stderr, "Usage: %s [-c INT] [-o OUTPUT_FILE] MATRIX_FILE\n", argv[0]);
+  bool valid_opts = true;
+  if(min_abund <= 0) {
+    valid_opts = false;
+    fprintf(stderr, "[error] -a should be a positive integer.\n");
+  }
+  if(min_samples <= 0) {
+    valid_opts = false;
+    fprintf(stderr, "[error] -c should be a positive integer.\n");
+  }
+  if(!valid_opts) {
     return 1;
   }
 
-  if(min_count <= 0) { 
-    fprintf(stderr, "[error] -c parameter should be an integer > 0\n", min_count);
-    return 1;
+  if(argc-optind != 1 || help_opt) {
+    fprintf(stdout, "Usage: km_basic_filter [options] <in.mat>\n\n");
+
+    fprintf(stdout, "Filter a matrix by selecting k-mers that are potentially differential.\n");
+    
+    fprintf(stdout, "Options:\n");
+    fprintf(stdout, "  -a INT   min abundance to define a k-mer as present in a sample [10]\n");
+    fprintf(stdout, "  -c INT   min number of samples for which a k-mer should be present/absent [10]\n");
+    fprintf(stdout, "  -o FILE  output filtered matrix to FILE [stdout]\n");
+    fprintf(stdout, "  -h       print this help message\n");
+    return 0;
   }
 
-  FILE *matfile = fopen(argv[optind],"r");
+  FILE *matfile = strcmp(argv[optind],"-") ? fopen(argv[optind],"r") : stdin;
   if(matfile == NULL) { 
     fprintf(stderr,"[error] cannot open file \"%s\"\n",argv[optind]);
     return 1;
@@ -46,8 +69,10 @@ int main(int argc, char **argv) {
 
   FILE *outfile = out_fname ? fopen(out_fname,"w") : stdout;
   if(outfile != stdout && outfile == NULL) {
-    fclose(matfile);
-    fprintf(stderr,"[error] cannot open output file \"%s\"\n",out_fname); return 1; }
+    if(matfile != stdin){ fclose(matfile); }
+    fprintf(stderr,"[error] cannot open output file \"%s\"\n",out_fname);
+    return 1;
+  }
 
   size_t n_samples = 0, n_kmers = 0, n_retrieved = 0;
 
@@ -72,10 +97,10 @@ int main(int argc, char **argv) {
         ++n_samples; 
       }
       long val = strtol(elem,NULL,10);
-      if(val == 0){ ++n_zeros; } else if(val >= min_count){ ++n_present; }
+      if(val == 0){ ++n_zeros; } else if(val >= min_abund){ ++n_present; }
     }
 
-    if(n_zeros >= 10 && n_present >= 10) {
+    if(n_zeros >= min_samples && n_present >= min_samples) {
       ++n_retrieved;
       fputs(line_cpy, outfile);
     }
@@ -86,13 +111,12 @@ int main(int argc, char **argv) {
     ch_read = getline(&line, &line_size, matfile);
   }
 
-  fprintf(stderr, "%u\tsamples\n", n_samples);
-  fprintf(stderr, "%u\tk-mers\n", n_kmers);
-  fprintf(stderr, "%u\tk-mers that are absent from at least 10 samples but present (abund >= %d) in at least 10 others\n", n_retrieved, min_count);
+  fprintf(stderr, "[info] %u\tsamples\n", n_samples);
+  fprintf(stderr, "[info] %u\tk-mers\n", n_kmers);
+  fprintf(stderr, "[info] %u\tretained k-mers (present/absent in >= %d samples, abundance >= %d)\n", n_retrieved, min_samples, min_abund);
 
-  free(line); 
-  free(line_cpy);
-  fclose(matfile);
+  free(line); free(line_cpy);
+  if(matfile != stdin){ fclose(matfile); }
   if(outfile != stdout){ fclose(outfile); }
 
   return 0;
