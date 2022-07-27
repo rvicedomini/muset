1739d75eea95d642fd50346a34f0d01ee5204c60
VICEDOMINI Riccardo <ric.vice@gmail.com>
2022-07-27 14:17:12 +0200

    minor polishing; added help message

--- km_select.c
+++ km_select.c
@@ -43,10 +43,10 @@ int main(int argc, char **argv) {
 
   int ksize = 31;
   char *out_fname = NULL;
-  bool do_select = true;
+  bool do_select = true, help_opt = false;
 
   int c;
-  while ((c = getopt(argc, argv, "k:o:v")) != -1) {
+  while ((c = getopt(argc, argv, "k:o:vh")) != -1) {
     switch (c) {
       case 'k':
         ksize = strtol(optarg, NULL, 10);
@@ -56,6 +56,9 @@ int main(int argc, char **argv) {
         break;
       case 'v':
         do_select = false;
+        break;
+      case 'h':
+        help_opt = true;
         break;
       case '?':
         return 1;
@@ -69,47 +72,62 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  if(argc-optind != 2) {
-    fprintf(stderr, "Usage: %s [options] kmer_file matrix_file\n", argv[0]);
+  if(argc-optind != 2 || help_opt) {
+    fprintf(stdout, "Usage: km_select [options] <matrix_1> <matrix_2>\n\n");
+    fprintf(stdout, "Select lines from <matrix_2> corresponding to k-mers belonging to <matrix_1>.\n");
+    fprintf(stdout, "Input matrices are assumed to be sorted by k-mer.\n\n");
+    fprintf(stdout, "Options:\n");
+    fprintf(stdout, "  -k INT   size of k-mers in the input matrices [31]\n");
+    fprintf(stdout, "  -o FILE  output reverse-complement matrix to FILE [stdout]\n");
+    fprintf(stdout, "  -v       select k-mers that DO NOT belong to <matrix_1>\n");
+    fprintf(stdout, "  -h       print this help message\n");
+    return 0;
+  }
+
+  FILE *selfile = fopen(argv[optind],"r");
+  if(selfile == NULL) {
+    fprintf(stderr,"Cannot open file \"%s\"\n",argv[optind]);
     return 1;
   }
 
-  FILE *kfile = fopen(argv[optind],"r");
-  if(kfile == NULL) { fprintf(stderr,"Cannot open file \"%s\"\n",argv[optind]); return 1; }
-
   FILE *matfile = fopen(argv[optind+1],"r");
-  if(matfile == NULL) { fclose(kfile); fprintf(stderr,"Cannot open file \"%s\"\n",argv[optind+1]); return 1; }
+  if(matfile == NULL) {
+    fprintf(stderr,"Cannot open file \"%s\"\n",argv[optind+1]);
+    fclose(selfile); 
+    return 1;
+  }
 
   FILE *outfile = out_fname ? fopen(out_fname,"w") : stdout;
-  if(outfile != stdout && outfile == NULL) { fclose(kfile); fclose(matfile); fprintf(stderr,"Cannot open output file \"%s\"\n",out_fname); return 1; }
+  if(outfile != stdout && outfile == NULL) {
+    fprintf(stderr,"Cannot open output file \"%s\"\n",out_fname);
+    fclose(selfile);
+    fclose(matfile);
+    return 1;
+  }
 
   char *sel_kmer = (char *)calloc(ksize+1,1);
   char *mat_kmer = (char *)calloc(ksize+1,1);
   char *line = NULL;
   size_t line_size = 0;
 
-  long mat_count = 0;
-
-  bool ret_sel = next_kmer(sel_kmer, ksize, kfile);
+  bool ret_sel = next_kmer(sel_kmer, ksize, selfile);
   bool ret_mat = next_kmer_and_line(mat_kmer, ksize, &line, &line_size, matfile);
   while(ret_sel && ret_mat){
     int ret_cmp = strcmp(sel_kmer, mat_kmer);
     if(ret_cmp == 0) {
       if(do_select){ fputs(line,outfile); }
-      ret_sel = next_kmer(sel_kmer, ksize, kfile);
+      ret_sel = next_kmer(sel_kmer, ksize, selfile);
       ret_mat = next_kmer_and_line(mat_kmer, ksize, &line, &line_size, matfile);
-      if(++mat_count % 16000000 == 0) { fprintf(stderr,"%ld processed\n",mat_count); }
     } else if(ret_cmp < 0) {
-      ret_sel = next_kmer(sel_kmer, ksize, kfile);
+      ret_sel = next_kmer(sel_kmer, ksize, selfile);
     } else { // ret_cmp > 0
       if(!do_select){ fputs(line,outfile); }
       ret_mat = next_kmer_and_line(mat_kmer, ksize, &line, &line_size, matfile);
-      if(++mat_count % 16000000 == 0) { fprintf(stderr,"%ld processed\n",mat_count); }
     }
   }
 
   free(line);
-  fclose(kfile);
+  fclose(selfile);
   fclose(matfile);
   if(outfile != stdout){ fclose(outfile); }
 
