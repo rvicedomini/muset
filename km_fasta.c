e5a71e2af100ced902bd0395ad25fb2a4138b304
VICEDOMINI Riccardo <ric.vice@gmail.com>
2022-07-27 11:24:45 +0200

    only valid k-mers are now outputted

--- km_fasta.c
+++ km_fasta.c
@@ -6,20 +6,39 @@
 #include <stdbool.h>
 #include <string.h>
 
+static const int isnuc[256] = {
+      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+      0,   1,   0,   1,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   1,   0,
+      0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+      0,   1,   0,   1,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   1,   0,
+      0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0
+};
+
 
 int main(int argc, char **argv) {
 
-  int ksize = 31;
   char *out_fname = NULL;
+  bool help_opt = false;
 
   int c;
-  while ((c = getopt(argc, argv, "k:o:")) != -1) {
+  while ((c = getopt(argc, argv, "o:")) != -1) {
     switch (c) {
-      case 'k':
-        ksize = strtol(optarg, NULL, 10);
-        break;
       case 'o':
         out_fname = optarg;
+        break;
+      case 'h':
+        help_opt = true;
         break;
       case '?':
         return 1;
@@ -28,46 +47,68 @@ int main(int argc, char **argv) {
     }
   }
 
-  if(ksize <= 0) { 
-    fprintf(stderr, "Invalid value of k: %d\n",ksize);
-    return 1;
+  if(argc-optind != 1 || help_opt) {
+    fprintf(stdout, "Usage: km_fasta [options] <in.mat>\n\n");
+
+    fprintf(stdout, "Output k-mers of a k-mer matrix in a FASTA file.\n");
+    
+    fprintf(stdout, "Options:\n");
+    fprintf(stdout, "  -o FILE  output FASTA file of k-mers to FILE [stdout]\n");
+    fprintf(stdout, "  -h       print this help message\n");
+    return 0;
   }
 
-  if(argc-optind != 1) {
-    fprintf(stderr, "Usage: %s [-k INT] [-o PATH] matrix_file\n", argv[0]);
+  FILE *fp = strcmp(argv[optind],"-") ? fopen(argv[optind],"r") : stdin;
+  if(fp == NULL) {
+    fprintf(stderr,"[error] cannot open file \"%s\"\n",argv[optind]);
     return 1;
   }
-
-  FILE *fp = fopen(argv[optind],"r");
-  if(fp == NULL) { fprintf(stderr,"Cannot open file \"%s\"\n",argv[optind]); return 1; }
 
   FILE *outfile = out_fname ? fopen(out_fname,"w") : stdout;
-  if(outfile != stdout && outfile == NULL) { fclose(fp); fprintf(stderr,"[error] cannot open output file \"%s\"\n",out_fname); return 1; }
+  if(outfile != stdout && outfile == NULL) {
+    if(fp != stdin){ fclose(fp); }
+    fprintf(stderr,"[error] cannot open output file \"%s\"\n",out_fname);
+    return 1;
+  }
 
   char *line = NULL;
-  size_t line_size=0, line_num=0, count=0;
+  size_t line_size=0, line_num=0, kmer_count=0;
   
   ssize_t ch_read = getline(&line, &line_size, fp);
   while(ch_read >= 0) {
     ++line_num;
-    
-    if(ch_read == 0 || (ch_read == 1 && line[0]=='\n')) { 
-      continue; 
-    } else if (ch_read < ksize) {
-      fprintf(stderr,"[error] cannot read a k-mer of size %d at line %zu\n", ksize, line_num);
-      free(line);
-      fclose(fp);
-      if(outfile != stdout){fclose(outfile);}
-      return 2;
+
+    if(ch_read == 0 || line[0]=='\n') { // skip empty lines
+      continue;
     }
 
-    line[ksize] = '\0';
-    fprintf(outfile, ">%zu\n%s\n", ++count, line);
+    bool valid_kmer = true;    
+    char *kmer = strtok(line," \t\n");
+    if(kmer == NULL){ 
+      fprintf(stderr,"[warning] invalid k-mer at line %zu\n", line_num);
+      valid_kmer = false;
+    }
+    else 
+    {
+      size_t ksize = strlen(kmer);
+      for(char *n=kmer; *n; ++n) {
+        if(!isnuc[*n]) {
+          fprintf(stderr,"[warning] invalid k-mer at line %zu: %s\n", line_num, line);
+          valid_kmer = false;
+        }
+      }
+    }
+
+    if(valid_kmer) {
+      fprintf(outfile, ">%zu\n%s\n", ++kmer_count, line);
+    }
+
     ch_read = getline(&line, &line_size, fp);
   }
 
+  fprintf(stderr, "[info] %zu k-mers written.\n", kmer_count);
   free(line);
-  fclose(fp);
+  if(fp != stdin) { fclose(fp); }
   if(outfile != stdout){ fclose(outfile); }
 
   return 0;
